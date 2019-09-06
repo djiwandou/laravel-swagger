6e5b5995f2e34c04c8b15c700ccc131bd9470e4c
Djiwandou Putro <djiwandou@tokodistributor.com>
2019-09-06 08:49:06 +0700

    edit L5_SWAGGER_CONST_HOST to herokuapp link

--- config/l5-swagger.php
+++ config/l5-swagger.php
@@ -232,6 +232,6 @@ return [
     |--------------------------------------------------------------------------
      */
     'constants' => [
-        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'http://my-default-host.com'),
+        'L5_SWAGGER_CONST_HOST' => env('L5_SWAGGER_CONST_HOST', 'https://tes-swagger.herokuapp.com'),
     ],
 ];
